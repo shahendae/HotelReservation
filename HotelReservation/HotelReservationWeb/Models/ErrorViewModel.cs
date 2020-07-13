ba08e36359b7962fe23dbb3be09ace1b1a4e01c9
shahende <shahenda.mena@gmail.com>
2020-07-13 23:06:37 +0200

    Initial Web files

---
+++ HotelReservation/HotelReservationWeb/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace HotelReservationWeb.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
